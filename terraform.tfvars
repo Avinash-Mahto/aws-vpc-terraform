204b7ad75c2c12f60d0ca94dd03f833e03934156
Avinash-Mahto <aklinuxworld@gmail.com>
2023-12-08 01:33:00 +0800

    Add files via upload

---
+++ terraform.tfvars
@@ -0,0 +1,34 @@
+# Define the Region
+region = "ap-southeast-1"
+# Avialability zone
+availability_zone1 = "ap-southeast-1a"
+# VPC CIDR range
+vpc_cidr = "10.15.0.0/16"
+# VPC Name
+vpc_name = "vpc-poc"
+# Web Subnet CIDR
+web_sub_cidr = "10.15.2.0/23"
+# Web subnet name
+web_sub_name = "web-subnet"
+# App Subnet CIDR
+app_sub_cidr = "10.15.4.0/23"
+# App subnet name
+app_sub_name = "app-subnet"
+# db Subnet CIDR
+db_sub_cidr = "10.15.6.0/23"
+# App subnet name
+db_sub_name = "app-subnet"
+# Public route name
+pub_route_name = "pub_route"
+# Public route name
+pri_route_name = "pri_route"
+# Internet Gateway
+my_igw_name = "poc_igw"
+# VPC Route CIDR
+ vpc_route_cidr = "0.0.0.0/0"
+# Elastic IP Name
+ elastic_ip_name =  "elip_poc"
+# NAT GW Name
+ my_natgw_name = "natgw_poc"
+# NAT GW CIDR
+natgw_cidr = "0.0.0.0/0"
---
+++ variables.tf
@@ -0,0 +1,51 @@
+variable "region" {
+    type = string
+}
+variable "availability_zone1" {
+    type = string
+}
+variable "vpc_cidr" {
+    type = string
+}
+variable "vpc_name" {
+    type = string
+}
+variable "web_sub_cidr" {
+    type = string
+}
+variable "web_sub_name" {
+    type = string
+}
+variable "app_sub_cidr" {
+    type = string
+}
+variable "app_sub_name" {
+    type = string
+}
+variable "db_sub_cidr" {
+    type = string
+}
+variable "db_sub_name" {
+    type = string
+}
+variable "pub_route_name" {
+    type = string
+}
+variable "pri_route_name" {
+    type = string
+}
+variable "my_igw_name" {
+    type = string
+}
+variable "vpc_route_cidr" {
+    type = string
+}
+variable "elastic_ip_name" {
+    type = string
+}
+variable "my_natgw_name" {
+    type = string
+}
+variable "natgw_cidr" {
+    type = string
+}
